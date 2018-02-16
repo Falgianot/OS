8e9daf7d7478d860b2db58aae4daecad61ab35a1
Tyler Falgiano <34319591+Falgianot@users.noreply.github.com>
2018-02-16 00:10:34 -0500

    able to run two threads

--- my_pthread.c
+++ my_pthread.c
@@ -10,15 +10,43 @@
 #include <ucontext.h>
 #include <signal.h>
 #include <unistd.h>
+#include <time.h>
 
 
 //defining global vars
 //4096
 #define STACK_SIZE 1024*64
-#define splice 25
-#define mutexSize 5
-static ucontext_t uctx_main;
+#define MAX_THREADS 64
+#define slice 2000
+#define priorities 5
+static ucontext_t * uctx_main;
+static ucontext_t * uctx_handler;
 int tid = 1;
+int isInit = 0;
+tcb * running_thread;
+struct itimerval timer;
+struct timeval interval;
+struct itimerval thread_timer;
+struct timeval thread_interval;
+int isHandler =0;
+int firstSwap =0;
+
+
+int pick [5]= {16, 8 ,6 ,4 ,2};
+int times [5] = {15000, 30000,45000, 50000, 55000};
+
+void pause_timer(){
+	struct itimerval zero_timer = {0};
+	setitimer(ITIMER_REAL, &zero_timer, &timer);
+	
+}
+
+void resume_timer(){
+	setitimer(ITIMER_REAL, &timer, NULL);
+	
+}
+
+
 
 
 
@@ -28,137 +56,332 @@ typedef struct Node{
 	
 }node;
 
-node** priority = NULL;
-my_pthread_mutex_t** waitQueue = NULL;
-int isInit = 0;
-int queueInit = 0;
+typedef struct Queue{
+	struct Node * front;
+	struct Node * tail;
+	int size;
+}queue;
+
+struct Queue *createQueue()
+{
+    struct Queue *q = (struct Queue*)malloc(sizeof(struct Queue));
+    q->front = q->tail = NULL;
+	q->size = 0;
+    return q;
+}
+
+queue * ready_queue;
+queue * waiting_queue;
+queue * running_queue;
+
+//priority array - scheduler
+ queue** priority = NULL;
+ node * current_thread;
+
+ 
+ //for exit and join
+ //done array - finished threads
+ tcb * done;
+ 
+void enqueue(int p, tcb * cb){
+		node * insert = (node *) malloc(sizeof(node));
+		cb->state = ready;
+		insert->thread = cb;
+		insert->next = NULL;
+		
+		queue * q = priority[p];
+		
+		if(q->front == NULL){
+			q->front = insert;
+			q->tail = insert;
+			
+		}else{
+			q->tail->next = insert;
+			q->tail = insert;
+		
+		}
+		q->size = q->size + 1;
+}
 
 
-//initializeScheduler
+
+ void enqueue_running(node * insert){
+		
+		
+		if(running_queue->front == NULL){
+			running_queue->front = insert;
+			running_queue->tail = insert;
+			
+		}else{
+			running_queue->tail->next = insert;
+			running_queue->tail = insert;
+		
+		}
+		running_queue->size = running_queue->size + 1;
+}
+
+struct Node * dequeue_running(){
+	if(running_queue->front == NULL){
+		return NULL;
+	}
+	
+	node * delete = running_queue->front;
+	running_queue->front = running_queue->front->next;
+	
+	if(running_queue->front == NULL){
+		running_queue->tail = NULL;
+	}
+	//what do we want to do with it
+	running_queue->size = running_queue->size - 1;
+	return delete;
+}
+struct Node * dequeue(int p){
+	queue * q = priority[p];
+	
+	if(q->front == NULL){
+		return NULL;
+	}
+	
+	node * delete = q->front;
+	q->front = q->front->next;
+	
+	if(q->front == NULL){
+		q->tail = NULL;
+	}
+	//what do we want to do with it
+	q->size = q->size - 1;
+	return delete;
+	
+}
+
+
+ void print_running(){
+	node * search = running_queue->front;
+	while(search!=NULL){
+		tcb * cb = search->thread;
+		printf("tid:%i\n",cb->tid);
+		search = search->next;
+	}
+}
+ 
+ 
+ void contextSwap(){
+	
+	//pause_timer();
+	//resume_timer();
+	
+	//swapping contexts
+	//while(running_queue->size>0){
+		//signal(SIGALRM,contextSwap);
+		//This block adjusts timer for different priorities.
+		node * temp = dequeue_running();
+		running_thread = temp->thread;
+		int running_priority = running_thread->priority;
+		
+		thread_interval.tv_sec = 0;
+		thread_interval.tv_usec = times[running_priority];
+			
+		thread_timer.it_interval = thread_interval;
+		thread_timer.it_value = thread_interval;
+		
+			
+		//setitimer(ITIMER_REAL,&thread_timer,NULL);
+		
+		/*Problem: Itimer from initialize runs for one interval
+		 *		then alarm goes off, and it never switches back to thread to finish the job.
+		 *		Longer time intervals means more increments, shorter means other problems.
+		 *		Needs to be long enough at least.
+		 */
+		
+		printf("im back\n");
+		
+		
+
+	
+	//}
+	printf("%d is now running\n", running_thread->tid);
+	swapcontext(uctx_main,running_thread->cxt);
+	//raise(SIGALRM);
+	//signal(SIGALRM,my_handler);
+	//resume_timer();
+ }
+ 
+ 
+ 
+ //pick 16,8,4,2,1
+ //try to only call swap once during function call
+void my_handler(int signum){
+	//so timer set in initialize works
+	/*if(firstSwap == 0){
+		firstSwap = 1;		
+		swapcontext(uctx_main,uctx_handler);
+	}*/
+	if(running_thread!=NULL){
+		//insert into appropriate queue...done or scheduler
+		printf("thread slice done\n");
+		//pause_timer();
+		enqueue(running_thread->priority, running_thread);
+		ucontext_t * temp = running_thread->cxt;
+		running_thread = NULL;
+		swapcontext(temp,uctx_main);
+		
+		
+		
+		//raise(SIGALRM);
+	}
+	else if(running_queue->size ==0){
+		int i =0;
+		while(i<priorities){
+			//Get number of threads we are picking at the priority level and enqueue into running queue
+			//If there aren't enough threads in that level just go to the next
+			int p = pick[i];
+			//queue * q = priority[i];
+			int k = 0;
+			while(priority[i]->size>0&&k<p){
+				node * node_leaving = dequeue(i);
+				enqueue_running(node_leaving);
+				k++;
+			}
+		i++;
+		}
+		contextSwap();
+	}
+	else{
+		
+		contextSwap();
+	}
+	
+	
+	
+		
+	
+}
+
+
 void initialize(){
 	if(isInit == 0){
-		priority = ( node **)malloc(sizeof( node *)*5);
+		done = (tcb *) malloc(sizeof(tcb*)*MAX_THREADS); //for threads that are done, and might be waited on
+		running_queue = createQueue(); 
+		waiting_queue = createQueue();
+		running_thread = NULL;
+		//ready_queue = createQueue();
+		current_thread = (node *) malloc(sizeof(node)); //thread we are currently executing for a certain time slice
+		signal(SIGALRM, my_handler); //linking our handler to the OS
+		priority = ( queue **)malloc(sizeof( queue *)*5); //our scheduler data structure. an array of queues
 		int i = 0;
-		
-		
-		while(i<5){
-			priority[i] = NULL;
+		while(i<priorities){
+			priority[i] = createQueue();
 			i++;
 		}
+		
+		
+		//get context of main
+		uctx_main = (ucontext_t *)malloc(sizeof(ucontext_t));
+		if(getcontext(uctx_main)==-1){
+		perror("getcontext failed");
+		exit(0);
+	}
+		void * stack = malloc(STACK_SIZE);
+		uctx_main->uc_stack.ss_sp = stack;
+		uctx_main->uc_stack.ss_size = STACK_SIZE;
+		uctx_main->uc_link = 0;
+		uctx_main->uc_stack.ss_flags=0;	
+		
+		//set up context of my_handler
+		//??double threads on same context
+		uctx_handler = (ucontext_t *)malloc(sizeof(ucontext_t));
+		if(getcontext(uctx_handler)==-1){
+		perror("getcontext failed");
+		exit(0);
+	}
+		//void * stack = malloc(STACK_SIZE);
+		uctx_handler->uc_stack.ss_sp = stack;
+		uctx_handler->uc_stack.ss_size = STACK_SIZE;
+		uctx_handler->uc_link = 0;
+		uctx_handler->uc_stack.ss_flags=0;	
+		//?? why does this exist
+		makecontext(uctx_handler,(void *)my_handler,1,SIGALRM);
+		
+		
+		//setting itimer
+		
+		interval.tv_sec = 0;
+		interval.tv_usec = slice;
+		
+		
+		timer.it_interval = interval;
+		timer.it_value = interval;
+		//?? this timer may or may not act within pthread create time
+		setitimer(ITIMER_REAL,&timer,NULL);
 		
 		
 		isInit = 1;
 	}else{
 		return;
 	}
-	
 }
 
-void queueInitialize(my_pthread_mutex_t *mutex){
+
+
+
+void print_schedule(){
 	int i = 0;
-	mutex->locked = 0;    //unlocked at start
-	mutex->isInit = 1;    //Initiliazed the lock with boolean 1
-	if(queueInit == 0){
-		waitQueue = ( my_pthread_mutex_t **)malloc(sizeof( my_pthread_mutex_t *)*mutexSize); //malloc size of max amount of locks	
-		while(i<5){
-			waitQueue[i] = NULL;
-			i++;
+	while(i<priorities){
+		queue * q = priority[i];
+		node * search = q->front;
+		while(search!=NULL){
+			tcb * cb = search->thread;
+			printf("Priority:%i \t Size:%i tid:%i\n",i,q->size,cb->tid);
+			search = search->next;
 		}
-		i=0;
-		while(i<5){
-			if(waitQueue[i]==NULL){   //initializing lock 
-				waitQueue[i] = mutex; 
-				break;
-				
-			}
-			i++;
-		}	
-		
-		queueInit = 1;
-	}
-	else{
-		i=0;
-		while(i<5){             //initializing lock 
-			if(waitQueue[i] == NULL){
-				waitQueue[i] = mutex; 
-				break;
-				
-			}
 		i++;
-		}		
 	}
-}
-//handles signals
-//signals used if time runs out or if pthread yield called.
-void my_handler(int signum){
-	if(signum==SIGUSR1){
-		//scheduler stuff
-		
-		
-	}
+
 }
 
-/*inserts into scheduler
-Params: p - priority level. cb - thread
-*/
-void enqueue(int p, tcb * cb){
-		node * insert = (node *) malloc(sizeof(node));
-		insert->thread = cb;
-		insert->next = NULL;
-		if(priority[p] == NULL){
-			priority[p] = insert;
-			
-			
-		}else{
-		
-		node *ptr = priority[p];
-		node * prev = ptr;
-		
-		while(ptr!=NULL){
-			prev = ptr;
-			ptr=ptr->next;
-		}
-		prev->next = insert;
-		}
-}
+
 
 /* create a new thread */
 int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg) {
-	
-	signal(SIGALRM, my_handler);
+	initialize();
 	
 	tcb *control_block = (tcb*)malloc(sizeof(tcb));
 	control_block->tid = tid;
 	tid++;
 	control_block->state = embryo;
 	
-	if(getcontext(&control_block->cxt)==-1){
+	
+	ucontext_t * c = (ucontext_t *)malloc(sizeof(ucontext_t));
+	
+	if(getcontext(c)==-1){
 		perror("getcontext failed");
 		exit(0);
 	}
 	
-	ucontext_t c = control_block->cxt;
+	
+	
+	
 	void * stack = malloc(STACK_SIZE);
-	c.uc_stack.ss_sp = stack;
-	c.uc_stack.ss_size = STACK_SIZE;
-	c.uc_link = &uctx_main;
-	c.uc_stack.ss_flags=0;
+	c->uc_stack.ss_sp = stack;
+	c->uc_stack.ss_size = STACK_SIZE;
+	c->uc_link = uctx_handler;
+	c->uc_stack.ss_flags=0;
 	control_block->stack = stack;
 	control_block->next = NULL;
 	thread[0]=control_block->tid;
 	
-	makecontext(&c,(void*)function,1,arg);
-	swapcontext(&uctx_main,&c);
-	//swapcontext(&c,&uctx_main);
+	makecontext(c,(void*)function,1,arg);
+	control_block->cxt = c;
+	
 
 		//must add timesplice and priority later
 		//count for error if insufficient stack space etc.
-		initialize();
 		enqueue(0, control_block);
-		
-		
+		//raise(SIGALRM);
 		
 		my_pthread_yield();
+		
+		//my_pthread_yield();
 	return 0;
 	};
 
@@ -171,31 +394,32 @@ int my_pthread_yield() {
 };
 
 /* terminate a thread */
+//make done array
+//set tcb ret val to value ptr
+//for join continue based on TID check, set value_ptr
+//add to complete just in case thread calls join
 void my_pthread_exit(void *value_ptr) {
+	printf("EXIT");
+	//free(running_thread->cxt);
+	
+	//running_thread == NULL;
+	setcontext(uctx_main);
+
 };
 
 /* wait for thread termination */
+
 int my_pthread_join(my_pthread_t thread, void **value_ptr) {
 	return 0;
 };
 
 /* initial the mutex lock */
 int my_pthread_mutex_init(my_pthread_mutex_t *mutex, const pthread_mutexattr_t *mutexattr) {
-	queueInitialize(mutex);
 	return 0;
 };
 
 /* aquire the mutex lock */
 int my_pthread_mutex_lock(my_pthread_mutex_t *mutex) {
-	int i = 0;
-		while(i<5){
-			if(waitQueue[i]==mutex){
-				waitQueue[i] = mutex; 
-				break;
-				
-			}
-			i++;
-		}	
 	return 0;
 };
 
@@ -208,4 +432,6 @@ int my_pthread_mutex_unlock(my_pthread_mutex_t *mutex) {
 int my_pthread_mutex_destroy(my_pthread_mutex_t *mutex) {
 	return 0;
 };
+
+
 
--- test.c
+++ test.c
@@ -4,14 +4,25 @@
 #include "my_pthread_t.h"
 
 my_pthread_t * threadArray;
-my_pthread_mutex_t a;
-my_pthread_mutex_t b;
-void * dummy(){
+//comment
+void * dummy1(){
 int j=0;
-for(j=0;j<10;j++){
+
+for(j=0;j<12500;j++){
+    
+    printf("%d\n",j);
+  
+	}
+
+    my_pthread_exit(0);
+}
+
+void * dummy2(){
+int j=0;
+for(j=12500;j<25000;j++){
     printf("%d\n",j);
 	}
-return 0;
+    my_pthread_exit(0);
 }
 
 int main(int argc, char** arg){
@@ -19,16 +30,21 @@ int main(int argc, char** arg){
 	//getcontext(&uctx_main);
     threadArray = (my_pthread_t *)malloc(sizeof(my_pthread_t)*10);
     int i;
-   for(i = 0; i< 2; i++){
-    //my_pthread_create(&threadArray[0],NULL,dummy,NULL);
-	my_pthread_mutex_init(&a,NULL);
-		my_pthread_mutex_init(&b,NULL);
-   }
-my_pthread_mutex_lock(&a);
-    /*int j = 0;
-    for(j = 0; j<5; j++){
-        printf("tid:%d\n",threadArray[j]);
-    }*/
+    
+	
+    my_pthread_create(&threadArray[0],NULL,dummy1,NULL);
+    printf("back in main\n");
+    my_pthread_create(&threadArray[1],NULL,dummy2,NULL);
+        printf("back in main\n");
+
+       while(1){
+            
+            
+        }
+   
+    
+    
+    
 	return 0;
 }
 
