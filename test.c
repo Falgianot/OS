7c456bdb5b8d4c9ba31c8de0d97b7d0a96c9e1e1
Tyler Falgiano <34319591+Falgianot@users.noreply.github.com>
2018-02-16 22:56:03 -0500

    Starting join and exit

--- test.c
+++ test.c
@@ -5,7 +5,6 @@
 
 my_pthread_t * threadArray;
 //comment
-
 void * dummy1(){
 int j=0;
 
@@ -14,18 +13,22 @@ for(j=0;j<12500;j++){
     printf("%d\n",j);
   
 	}
-
-    //my_pthread_exit(0);
+    int a = 1;
+    int * b = (int *)malloc(sizeof(int));
+    my_pthread_exit((void *)b);
    // return 0;
 }
 
 void * dummy2(){
 int j=0;
 //sleep(1);
-for(j=12500;j<19000;j++){
+for(j=12500;j<12501;j++){
     printf("%d\n",j);
 	}
-   //my_pthread_exit(0);
+    
+    int c = 2;
+    int * d = (int *)malloc(sizeof(int));
+    my_pthread_exit((void *)d);
   //  return 0;
 }
 
@@ -33,7 +36,8 @@ int main(int argc, char** arg){
 
 	//getcontext(&uctx_main);
     threadArray = (my_pthread_t *)malloc(sizeof(my_pthread_t)*10);
-    int i;
+    
+    
 	
     my_pthread_create(&threadArray[0],NULL,dummy1,NULL);
     printf("back in main\n");
@@ -41,14 +45,12 @@ int main(int argc, char** arg){
     my_pthread_create(&threadArray[1],NULL,dummy2,NULL);
     printf("back in main\n");
     
-
- /* i = 10;
-while(i >0){
-   i--;
-  //printf("main\n");
-    
-}*/
+    int i = 1000000000000000000;
+while(i>0){
+    i--;
+}
      
+      
 	return 0;
 }
 
